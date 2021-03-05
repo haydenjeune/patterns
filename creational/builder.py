24a39f3a81e797f1fc67fc982e891d76ad9f2841
haydenjeune <33794706+haydenjeune@users.noreply.github.com>
2021-03-05 16:16:30 +1300

    Show how you can use builders to construct things without a shared interface

--- creational/builder.py
+++ creational/builder.py
@@ -3,7 +3,7 @@ The builder pattern utilises a builder object
 """
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, Optional
 
 
@@ -27,7 +27,7 @@ class Builder(ABC):
 
 @dataclass
 class API:
-    endpoints: dict = {}
+    endpoints: dict = field(default_factory=dict)
     auth_token: Optional[str] = None
 
     def get(self, path: str, token: str = None) -> str:
@@ -57,12 +57,40 @@ class APIBuilder(Builder):
         return self.api
 
 
-if __name__ == "__main__":
-    builder = APIBuilder()
+class APIDocsBuilder(Builder):
+    def __init__(self):
+        self.auth_docs = "Authorisation is not required for this API"
+        self.endpoint_docs = "Endpoints:\n"
+
+    def add_auth(self, token: str):
+        self.auth_docs = (
+            f"Authorisation is required for this API, with the token {token}"
+        )
+
+    def add_endpoint(self, path: str, response: str):
+        self.endpoint_docs += f"{path} -> {response}\n"
+
+    def add_healthcheck(self):
+        self.add_endpoint("healthcheck", "OK")
+
+    def finalise(self) -> str:
+        return self.auth_docs + "\n" + self.endpoint_docs
+
+
+def build_my_api(builder: Builder):
     builder.add_auth("fake_token")
     builder.add_endpoint("test", "this is the test endpoint")
     builder.add_healthcheck()
-    api = builder.finalise()
+    return builder.finalise()
+
+
+if __name__ == "__main__":
+    builder = APIBuilder()
+    api = build_my_api(builder)
 
     api.get("test", "fake_token")  # returns "this is the test endpoint"
     api.get("healthcheck", "fake_token")  # returns "OK"
+
+    builder = APIDocsBuilder()
+    docs = build_my_api(builder)  # using same function to construct docs
+    print(docs)
